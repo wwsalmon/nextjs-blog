b87beb273c157a38fc2b7a2385cf45347da5eb6c
Samson Zhang <wwsamson@yahoo.com>
2020-07-15 00:00:35 -0400

    add content back in

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Samson Zhang'
+export const siteTitle = 'SZ Next.js Sample Website'
 
 export default function Layout({
     children,
